5925fd301d17eb34ec1d1b70a68f7fdb300f90e0
Pushock <pzabolotnyi@gmail.com>
2023-03-09 23:01:51 +0300

    first committ

---
+++ vue-learning/online-shop/src/components/v-cart-item.vue
@@ -0,0 +1,58 @@
+<template lang="">
+    <div class="v-cart-item">
+        <img :src="require('../assets/images/' + cart_item_data.image)" alt="img">
+        <div class="v-cart-item__info">
+            <p>{{cart_item_data.name}}</p>
+            <p>Price: {{cart_item_data.price}}</p>
+            <p>{{cart_item_data.article}}</p>
+        </div>
+        <div class="v-cart-item__quantity">
+            <p>Quantity:</p>
+            {{cart_item_data.quantity}}
+        </div>
+        <button 
+            class="v-cart-item__btn"
+            @click="deleteFromCart">
+            Delete
+        </button>
+    </div>
+</template>
+<script>
+export default {
+    name: 'v-cart-item',
+    props: {
+        cart_item_data: {
+            type: Object,
+            default() {
+                return {}
+            }
+        }
+    },
+    methods: {
+        deleteFromCart() {
+            this.$emit('deleteFromCart')
+        }
+    },
+    mounted() {
+        this.$set(this.cart_item_data, 'quantity', 1)
+    }
+
+}
+</script>
+<style lang="scss">
+    .v-cart-item {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        flex-wrap: nowrap;
+        box-shadow: 0 0 8px 0 #e0e0e0;
+        padding: $padding*2;
+        margin-bottom: $margin*2;
+        img {
+            max-width: 80px;
+        }
+        &__btn {
+            @extend %btn;
+        }
+    }
+</style>
---
+++ vue-learning/online-shop/src/components/v-cart.vue
@@ -0,0 +1,45 @@
+<template lang="">
+    <div class="v-cart">
+        <router-link :to="{name: 'catalog'}">
+            <div class="v-catalog__link_to_cart btn">Back to Catalog</div>
+        </router-link>
+        <h1>Cart</h1>
+        <p v-if="!cart_data.length">There are no products in cart...</p>
+        <v-cart-item 
+        v-for="(item, index) in cart_data"
+        :key="item.article"
+        :cart_item_data="item"
+        @deleteFromCart="deleteFromCart(index)"
+        />
+    </div>
+</template>
+<script>
+import vCartItem from './v-cart-item.vue';
+import { mapActions } from 'vuex';
+
+export default {
+    name: 'v-cart',
+    components: {
+        vCartItem
+    },
+    props: {
+        cart_data: {
+            type: Array,
+            default() {
+                return []
+            }
+        }
+    },
+    methods: {
+        deleteFromCart(index) {
+            this.DELETE_FROM_CART(index)
+        },
+        ...mapActions([
+            'DELETE_FROM_CART'
+        ]) 
+    }
+}
+</script>
+<style lang="">
+    
+</style>
